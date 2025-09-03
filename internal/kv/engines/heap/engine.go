9338fc9cc8eb108c061a6c500b8255e04dc32b74
Anatolii Platonov <platonoff.dev@gmail.com>
2025-09-03 18:41:32 +0400

    Fix file page manager

---
+++ internal/kv/engines/heap/engine.go
@@ -0,0 +1,23 @@
+package heap
+
+import "errors"
+
+type HeapEngine struct {
+	fileManager
+}
+
+func NewHeapEngine() *HeapEngine {
+	return &HeapEngine{}
+}
+
+func (e *HeapEngine) Put(key []byte, value []byte) error {
+	return errors.New("not implemented")
+}
+
+func (e *HeapEngine) Get(key []byte) ([]byte, error) {
+	return nil, errors.New("not implemented")
+}
+
+func (e *HeapEngine) Delete(key []byte) error {
+	return errors.New("not implemented")
+}
