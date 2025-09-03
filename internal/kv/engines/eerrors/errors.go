85648143136b03ea035f342017b053fe76e61c27
Anatolii Platonov <platonoff.dev@gmail.com>
2025-09-03 18:22:04 +0400

    Reorganize again. Write basic common tests for kv storage engine

---
+++ internal/kv/engines/eerrors/errors.go
@@ -0,0 +1,7 @@
+package eerrors
+
+import "errors"
+
+var (
+	ErrKeyNotFound = errors.New("key not found")
+)
--- pkg/corekv/engines/engine.go
+++
@@ -1,8 +0,0 @@
-package engines
-
-type Engine interface {
-	Scan() ([][][]byte, error)
-	Get(k []byte) ([]byte, error)
-	Put(k []byte, v []byte) error
-	Delete(k []byte) error
-}
